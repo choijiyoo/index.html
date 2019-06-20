e826dfb391406aefe42a832b7e2e6c33d032ed86
choijiyoo <51592657+choijiyoo@users.noreply.github.com>
2019-06-20 13:05:50 +0900

    Add files via upload

---
+++ name_web1/js/myscript.js
@@ -0,0 +1,13 @@
+ // $(function(){
+ //      $(".drop-menu li").hover(function(){
+ //        $('ul:first',this).show();
+ //      }, function(){
+ //        $('ul:first',this).hide();
+ //      });
+ //      $(".drop-menu>li:has(ul)>a").each( function() {
+ //        $(this).html( $(this).html()+' &or;' );
+ //      });
+ //      $(".drop-menu ul li:has(ul)")
+ //        .find("a:first")
+ //        .append("<p style='float:right;margin:-3px'>&#9656;</p>");
+ //    });
